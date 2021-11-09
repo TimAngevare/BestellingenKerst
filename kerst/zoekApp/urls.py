b918583d970dbe0f33f18b361c164224a864f109
Tim Angevare <t.angevare.uu.nl>
2021-11-09 17:37:41 +0100

    Forms in bestellingen maken

--- kerst/zoekApp/urls.py
+++ kerst/zoekApp/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'nieuwApp'
 urlpatterns = [
     path('', views.zoek, name='zoek'),
+    path("bestellingen/", views.bestellingen, name="Bestellingen"),
+    path("producten/", views.producten, name="Bestellingen")
 ]
--- kerst/zoekApp/views.py
+++ kerst/zoekApp/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib import messages
 from . import forms
 
 def zoek(request):
-    bestel_form = forms.Bestellingen()
-    return render(request, 'zoekApp/zoek.html', {"bestel_form":bestel_form})
+    return render(request, 'zoekApp/zoek.html')
 
 def bestellingen(request):
-    pass
+    if request.method == "POST":
+        bestel_form = forms.Bestellingen(request.POST)
+        if bestel_form.is_valid():
+            num = bestel_form.cleaned_data["bestel_nmr"]
+            tel = bestel_form.cleaned_data["tel"]
+            dag = bestel_form.cleaned_data["dag_ophalen"]
+            print(num, tel, dag)
+        else:
+            messages.error(request, "Hier klopt iets niet, vul opnieuw in")
+        return HttpResponse("zoekApp/bestellingen_results.html")
+    else:
+        bestel_form = forms.Bestellingen()
+    return render(request, 'zoekApp/bestellingen.html', {"bestel_form":bestel_form})
 
 def producten(request):
     pass
