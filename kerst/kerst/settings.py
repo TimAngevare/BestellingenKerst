d0bd4c5cfdc3832b056a4b08931d022958e9126f
Jelle van den Brink <jelle@famvandenbrink.net>
2021-11-10 16:07:32 +0100

    meeting 10-11

--- kerst/kerst/settings.py
+++ kerst/kerst/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'nieuwApp.apps.NieuwappConfig',
-    #'zoekApp.apps.ZoekappConfig',
+    'zoekApp.apps.ZoekappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- kerst/zoekApp/urls.py
+++ kerst/zoekApp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'nieuwApp'
+app_name = 'zoekApp'
 urlpatterns = [
     path('', views.zoek, name='zoek'),
     path("bestellingen/", views.bestellingen, name="Bestellingen"),
