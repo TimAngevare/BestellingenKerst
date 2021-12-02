0810987ed5e6d58d9a75119a0653a40ea0f5742f
Jelle van den Brink <jelle@famvandenbrink.net>
2021-12-02 20:39:05 +0100

    veiligheid fixen

--- kerst/utils.py
+++ kerst/utils.py
@@ -1,8 +1,10 @@
 from pymongo import MongoClient
 import certifi
+import os
 
 
 def get_db(db_name):
-    connection_string = 'mongodb+srv://kerstAdmin:kerstAdmin123@kerst1.1cyvc.mongodb.net/kerst_db?retryWrites=true&w=majority'
+    # connection_string = 'mongodb+srv://kerstAdmin:kerstAdmin123@kerst1.1cyvc.mongodb.net/kerst_db?retryWrites=true&w=majority'
+    connection_string = os.environ['MONGODB_CONNECTION']
     client = MongoClient(connection_string, tlsCAFile=certifi.where())
     return client[db_name]
